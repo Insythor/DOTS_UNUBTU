fcbf1ec8dd6cc4153d80ad77e0967de5d7d80a17
just2720 <just2720@mailhost.cs.uleth.ca>
2020-02-15 10:28:45 -0700

    merging

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -1,6 +1,6 @@
 
 #include "screen.h"
-#include "dataManager.h"
+
 #include "dice.h"
 #include "player.h"
 
@@ -8,7 +8,6 @@
 int main()
 {
 	screen myScreen;
-  dataManager data;
 
 	myScreen.addToScreen(data.nextRoom(), 0, 0);
 
