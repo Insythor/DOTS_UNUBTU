ee802fdebce550282af1999e19ac0d3458b407da
Tyler (Tyler) JustinenTeite <just2720@36drax.cs.uleth.ca>
2020-02-17 12:28:56 -0700

    weapon now adds names

--- src/weapon.cpp
+++ src/weapon.cpp
@@ -2,16 +2,42 @@
 
 weapon::weapon()
 {
-  // Initiate the random seed for generating weapons
   srand(time(NULL));
+
+  readInNames();
+
 }
 
 weapon::~weapon()
 {
-  //dtor
+  delete allNames;
 }
 
-weapon weapon::generateWeapon(int level)
+void weapon::readInNames()
+{
+  allNames = new std::vector<std::string>;
+
+  std::ifstream weaponNames;
+  weaponNames.open(DIR_WEAPON);
+  std::string line;
+
+  while(getline(weaponNames, line))
+  {
+    allNames->push_back(line);
+  }
+
+  weaponNames.close();
+
+  allNames->shrink_to_fit();
+/**
+  for(auto i : (*allNames))
+  {
+    std::cout << i << std::endl;
+  }
+  */
+}
+
+void weapon::generateWeapon(int level)
 {
   if(level + 1 < 5)
   {
@@ -21,32 +47,43 @@ weapon weapon::generateWeapon(int level)
   {
     diceRolls = 4;
   }
-
   // Base + 4, then add + 0->4
   diceSize = level/2 + 4 + (rand() % 5);
 
-  std::ifstream weaponNames;
-  weaponNames.open(DIR_WEAPON);
-  std::string line;
-
- // srand(time(NULL));
-  int randLineNumber = rand() % 11;
-  int lineCounter = 1;
-
-  while(getline(weaponNames, line))
+  if(allNames != nullptr)
   {
-    if(lineCounter % randLineNumber == 0)
-    {
-      name = line;
-
-      return (*this);
-    }
-    lineCounter++;
+    name = allNames->at(rand() % allNames->size());
+    name.append(addType(diceSize));
   }
 
-  //name = "N/A";
+  else
+  {
+    name = "invalid";
+  }
+}
 
-  return (*this);
+std::string weapon::addType(int dSize)
+{
+  if(dSize == 4)
+    return " Dagger";
+  else if (dSize == 5)
+    return " Short-Sword";
+  else if (dSize == 6)
+    return " Sword";
+  else if (dSize == 7)
+    return " Flail";
+  else if (dSize == 8)
+    return " Two-Handed Axe";
+  else if (dSize == 9)
+    return " Whip";
+  else if (dSize == 10)
+    return " Halberd";
+  else if (dSize == 11)
+    return " Morning-Star";
+  else if (dSize == 12)
+    return " Scythe";
+  else
+    return " Invalid";
 }
 
 std::ostream& operator << (std::ostream &out, weapon &toRender)
