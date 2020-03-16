88fa4c68248029d00409587ec0a331b885cbb8ff
casd2720 <don.castillo@uleth.ca>
2020-03-16 12:20:51 -0600

    fixed file path

--- src/puzzleManager.cpp
+++ src/puzzleManager.cpp
@@ -63,14 +63,14 @@ void puzzleManager::puzzleThree() {
 
 // Puzzle 4
 void puzzleManager::puzzleFour() {
-    PuzzleFour *puzzle4 = new PuzzleFour("puzzle4-objects.tsv", 15);
+    PuzzleFour *puzzle4 = new PuzzleFour("../docs/DATA/puzzle4-objects.tsv", 15);
     puzzle4->mainGame();
 }
 
 
 // Puzzle 5
 void puzzleManager::puzzleFive() {
-    PuzzleFour *puzzle5 = new PuzzleFour("puzzle5-objects.tsv", 11);
+    PuzzleFour *puzzle5 = new PuzzleFour("../docs/DATA/puzzle5-objects.tsv", 11);
     puzzle5->mainGame();
 }
 
